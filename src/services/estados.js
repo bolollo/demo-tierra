ca33978bd7e8b2f1038e3833a7a38340865d83b6
Wladimir Szczerban <bolosig@gmail.com>
2020-12-20 14:15:59 +0100

    update estados query

--- src/services/estados.js
+++ src/services/estados.js
@@ -7,7 +7,7 @@ class EstadosService {
 
 	static async getEstados() {
 
-		const SQL = "SELECT nombre_igg, p_total FROM public.estados";
+		const SQL = "SELECT ogc_fid, nombre_igg, p_total FROM public.estados order by p_total DESC";
 
 		const {rows} = await db.query(SQL);
 
